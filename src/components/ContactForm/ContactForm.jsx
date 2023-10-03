84bb87825f10ba1fe5af3477625905b9c91a5ecd
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2023-10-03 16:04:13 +0300

    update back to start

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addContact } from 'redux/contactsSlice';
-import { getVisibleContacts } from 'redux/selectors';
-import { FormInput, ErrorMsg, AddButton } from './ContactForm.styled';
+import { Title, FormInput, ErrorMsg, AddButton } from './ContactForm.styled';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
@@ -18,67 +14,35 @@ const SubmitSchema = Yup.object().shape({
     .required('Required'),
 });
 
-export const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-
-  const contacts = useSelector(getVisibleContacts);
-  const dispatch = useDispatch();
-
-  const handleSubmit = event => {
-    event.preventDefault();
-
-    const isAlreadyExist = contacts.some(
-      contact =>
-        contact.name.toLowerCase().trim() === name.toLocaleLowerCase().trim()
-    );
-    if (isAlreadyExist) {
-      alert(`${name} is already in contacts`);
-      return;
-    }
-
-    dispatch(addContact({ name, number }));
-    setName('');
-    setNumber('');
-  };
-
-  const handleChange = event => {
-    const { name, value } = event.target;
-
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-      case 'number':
-        setNumber(value);
-        break;
-      default:
-        return;
-    }
-  };
-
+export const ContactForm = ({ title, onAdd }) => {
   return (
-    <Formik
-      onSubmit={handleSubmit}
-      initialValues={{ name: '', number: '' }}
-      validationSchema={SubmitSchema}
-    >
-      <Form>
-        <FormInput
-          type="text"
-          name="name"
-          placeholder="Full Name (Example: Pavlo Lysiuk)"
-        />
-        <ErrorMsg name="name" component="p" />
-        <FormInput
-          onChange={handleChange}
-          type="tel"
-          name="number"
-          placeholder="Phone number (Example: XXXXXXX)"
-        />
-        <ErrorMsg name="number" component="p" />
-        <AddButton type="submit">Add contact</AddButton>
-      </Form>
-    </Formik>
+    <div>
+      {title && <Title>{title}</Title>}
+
+      <Formik
+        initialValues={{ name: '', number: '' }}
+        validationSchema={SubmitSchema}
+        onSubmit={(values, actions) => {
+          onAdd(values);
+          actions.resetForm();
+        }}
+      >
+        <Form>
+          <FormInput
+            type="text"
+            name="name"
+            placeholder="Full Name (Example: Pavlo Lysiuk)"
+          />
+          <ErrorMsg name="name" component="p" />
+          <FormInput
+            type="tel"
+            name="number"
+            placeholder="Phone number (Example: XXXXXXX)"
+          />
+          <ErrorMsg name="number" component="p" />
+          <AddButton type="submit">Add contact</AddButton>
+        </Form>
+      </Formik>
+    </div>
   );
 };
